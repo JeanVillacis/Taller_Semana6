22bc52882c3ecdad033875486059e831efe9b3dc
Alexis Borges <alexis.borges@estudiantes.utec.edu.uy>
2026-03-24 16:41:36 -0300

    chore: configuracion inicial del proyecto base Spring Boot, docker, y db config (HT-001, HT-002)

---
+++ src/main/java/com/taller/semana6/taller_semana6/TallerSemana6Application.java
@@ -0,0 +1,13 @@
+package com.taller.semana6.taller_semana6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerSemana6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerSemana6Application.class, args);
+	}
+
+}
